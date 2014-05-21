085a0da21b11476b44e608ce0a45c86bd3c48484
Josh <josh@josh-VirtualBox.(none)>
2014-05-21 13:20:38 +1000

    asdasd

--- transfer_stats.py
+++ transfer_stats.py
@@ -139,19 +139,18 @@ def doWork( args ):
             genomes[hit[TP._IMG_ID_1]]={}
             genomes[hit[TP._IMG_ID_2]]={}
             try:
-               genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]["transfer_length"]+=hit[TP._LEN_1]
-               genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]["hits"]+= 1
+                genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]["transfer_length"]+= hit[TP._LEN_1]
+                #genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]["hits"]+= 1
             except KeyError:
                 try:
                     genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]["transfer_length"]= hit[TP._LEN_1]
-                    genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]["hits"]= 1
+                    #genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]["hits"]= 1
                 except KeyError:
-                    try:
-                        genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]={"transfer_length": hit[TP._LEN_1],"hits":1}
+                    try: 
+                        genomes[hit[TP._IMG_ID_1]][hit[TP._IMG_ID_2]]={"transfer_length":hit[TP._LEN_1]}
                     except KeyError:
-                        genomes[hit[TP._IMG_ID_1]]={hit[TP._IMG_ID_2]:{"transfer_length": hit[TP._LEN_1],"hits":1}}
-                        
-    
+                        genomes[hit[TP._IMG_ID_1]]={hit[TP._IMG_ID_2]:{"transfer_length":hit[TP._LEN_1]}}
+                    
     for key in genomes.keys():
         print key
         print genomes[key]                 
