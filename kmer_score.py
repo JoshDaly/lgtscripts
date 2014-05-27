30b2406c5af3bfe44bddaf2057759827e818a1ef
Josh <josh@josh-VirtualBox.(none)>
2014-05-27 16:17:45 +1000

    zsx

--- kmer_score.py
+++ kmer_score.py
@@ -152,6 +152,7 @@ def doWork( args ):
                 else:
                     g1_tmp_array.append([float(i) for i in fields[1:]]) # misses first element 
             g1_tmer = np.mean(g1_tmp_array, axis=0)
+            print g1_tmer
             LGT_dict[LGT_id].addGenomeTmer(GID1,g1_tmer)
         
         #-----
@@ -165,9 +166,10 @@ def doWork( args ):
                 else:
                     g2_tmp_array.append([float(i) for i in fields[1:]]) # misses first element 
             g2_tmer = np.mean(g1_tmp_array, axis=0)
+            print g2_tmer
             LGT_dict[LGT_id].addGenomeTmer(GID2,g2_tmer)
         
-        LGT_dict[LGT_id].getClosestGID()
+        #LGT_dict[LGT_id].getClosestGID()
          
     
     
