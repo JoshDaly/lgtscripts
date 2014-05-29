5a8604438f1e5829adde798542696f1e14c4d262
Josh <josh@josh-VirtualBox.(none)>
2014-05-30 09:54:08 +1000

    copy push

--- Add_uid_to_transfers_file.py
+++ Add_uid_to_transfers_file.py
@@ -137,7 +137,7 @@ class uidInfoDatabase(object):
             
     def matchUID(self,contig,img_id,start,stop):
         for uid in self.UID_dict.keys():
-            return UID
+            return uid
             #return (uid,self.UID_dict[uid].contig,self.UID_dict[uid].img_id_1,self.UID_dict[uid].start,self.UID_dict[uid].stop)
             #if self.UID_dict[uid].contig == contig and self.UID_dict[uid].img_id_1 == img_id and self.UID_dict[uid].start == start and self.UID_dict[uid].stop == stop:
                 
