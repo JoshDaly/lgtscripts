c5edde62c4c5bad01145e396195c045dab5d28eb
josh daly <joshua.daly@uqconnect.edu.au>
2014-06-01 14:04:13 +1000

    copy push

--- split_multi_fasta_to_individual.py
+++ split_multi_fasta_to_individual.py
@@ -100,10 +100,12 @@ def doWork( args ):
             #print header
             makeDirIfNotExist(uid_directory)
             makeFileIfNotExist(uid_file_dir)
+            print sequence.seq
             with open(uid_file_dir, 'w') as fh:
+                pass
                 #fh.write("hello world")
-                fh.write(header)
-                fh.write(sequence.seq)
+                #fh.write(header)
+                #fh.write(sequence.seq)
             counter +=1 
         """
         os.path.
