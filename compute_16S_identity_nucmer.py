9c6ed5754109d020ca801c0623fc6eddaa04464a
Josh <josh@josh-VirtualBox.(none)>
2014-06-05 10:00:11 +1000

    copy push

--- compute_16S_identity_nucmer.py
+++ compute_16S_identity_nucmer.py
@@ -39,8 +39,8 @@ import glob
 from multiprocessing import Pool
 from subprocess import Popen, PIPE
 
-#import os
-#import errno
+import os
+import errno
 
 #import numpy as np
 #np.seterr(all='raise')
@@ -78,6 +78,10 @@ def returnGenomeName(fasta_file):
     genome_name = slashes[-1].split(".")[0]
     return genome_name
 
+def doesDirectoryExist(output_dir):
+    if not os.path.exists(output_dir):
+        os.system("mkdir %s" % (output_dir))
+
 def doWork( args ):
     """ Main wrapper"""
     
@@ -86,16 +90,21 @@ def doWork( args ):
     pool = Pool(6)                              # 6 threads
     cmds = []
     fasta_files = glob.glob('%s/*/*.fna' % args.fasta_directory)
-
-    for i in range(len(fasta_files)-1):                    # Mikes example commands for running the script
-        for j in range(i+1, len(fasta_files)): # +1 and -1 to the for loops, means that only the bottom half of the triangle will be compared.
-            genome_1 = returnGenomeName(fasta_files[i])
-            genome_2 = returnGenomeName(fasta_files[j]) 
-            cmds.append("nucmer %s %s --mum --coords -p %s" % (fasta_files[i], fasta_files[j], "%s_v_%s" %(genome_1,genome_2)))
-        #if count == 1 or 2 or 3 or 4 or 5: # print current time after checkpoint
-         #   print datetime.datetime.now()
-        #count += 1 
-    print cmds
+    
+    if count < 10:
+        for i in range(len(fasta_files)-1):                    # Mikes example commands for running the script
+            for j in range(i+1, len(fasta_files)): # +1 and -1 to the for loops, means that only the bottom half of the triangle will be compared.
+                genome_1 = returnGenomeName(fasta_files[i])
+                genome_2 = returnGenomeName(fasta_files[j])
+                output_directory = "/srv/projects/lgt/img_4.1_all_lgt/16S_nucmer_97/%s_v_%s" % (genome_1,genome_2)
+                doesDirectoryExist(output_directory)
+                os.chdir(output_directory)
+                os.system("touch killme")
+                cmds.append("nucmer %s %s --mum --coords -p %s" % (fasta_files[i], fasta_files[j], "%s_v_%s" %(genome_1,genome_2)))
+            #if count == 1 or 2 or 3 or 4 or 5: # print current time after checkpoint
+             #   print datetime.datetime.now()
+            count += 1 
+    #print cmds
     #print pool.map(runCommand, cmds)            # list of tuples [(stdout, stderr)]
     
     
