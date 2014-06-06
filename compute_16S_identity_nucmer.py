a608f4829c59207deb5fc679a7ab0478db715812
Josh <josh@josh-VirtualBox.(none)>
2014-06-06 10:36:49 +1000

    copy push

--- compute_16S_identity_nucmer.py
+++ compute_16S_identity_nucmer.py
@@ -85,7 +85,9 @@ def doesDirectoryExist(output_dir):
 def runJobs((output_directory,fasta_1,fasta_2,genome_1,genome_2)):
     doesDirectoryExist(output_directory)
     os.chdir(output_directory)
-    call("nucmer %s %s --mum --coords -p %s 2>&1 >/dev/null" % (fasta_1, fasta_2, "%s_v_%s" %(genome_1,genome_2)),shell=True)
+    fnull = open(os.devnull,"w")
+    call("nucmer %s %s --mum --coords -p %s 2>&1 >/dev/null" % (fasta_1, fasta_2, "%s_v_%s" %(genome_1,genome_2)),stdout=fnull)
+    
 
 def doWork( args ):
     """ Main wrapper"""
