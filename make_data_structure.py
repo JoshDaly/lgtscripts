06154d708c489e6e06b51871d9f3a4e6d9898348
Josh <josh@josh-VirtualBox.(none)>
2014-06-06 12:20:05 +1000

    copy push

--- make_data_structure.py
+++ make_data_structure.py
@@ -94,7 +94,7 @@ def doWork( args ):
             count = 0
         genomes[genome] = alpha[i]
         print "\t".join([genome, genomes[genome]])
-    
+        count=+1
                 
             
             
