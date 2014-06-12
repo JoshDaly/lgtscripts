919e56f0561109b0c057e15f49fc9caf5e223492
Josh <josh@josh-VirtualBox.(none)>
2014-06-12 11:01:48 +1000

    copy push

--- kmer_score_multiplex-temp.py
+++ kmer_score_multiplex-temp.py
@@ -290,10 +290,10 @@ def doWork( args ):
                         GID_tmer = np.mean(GID_tmp_array, axis=0)
                         LGT_kmers.addGenomeTmer(GID, GID_tmer)
             count +=1 
-            if count >=100:
+            if count >=1000:
                 break
                 #pass
-        if count >=100:
+        if count >=1000:
                 break
                 #pass
         
