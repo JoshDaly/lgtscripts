226e782fcb08e14aedef716df83e460b72294f29
Josh <josh@josh-VirtualBox.(none)>
2014-06-23 16:42:22 +1000

    copy push

--- ANI_stats.py
+++ ANI_stats.py
@@ -147,7 +147,7 @@ def doWork( args ):
             ANI.addPAIR(l, uid)
             ANI.addScores(l, uid)
             uid +=1
-            if count_break >= 1000000:
+            if count_break >= 1000000000:
                 break
             count_break +=1  
             
